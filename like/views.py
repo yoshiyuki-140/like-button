d70296e806d833dd270a943712ca97f83d3dd628
Yoshiyuki Kurose <c1221251@st.kanazawa-it.ac.jp>
2024-02-05 12:36:59 +0900

    add : templates and viewを追加

--- like/views.py
+++ like/views.py
@@ -1,3 +1,14 @@
+#coding:utf-8
 from django.shortcuts import render
+import django_filters
+from rest_framework import viewsets, filters
 
-# Create your views here.
+from like.models import Object
+from like.serializer import ObjectSerializer
+
+
+class ObjectViewSet(viewsets.ModelViewSet):
+    """ViewSet for the Object class
+    """
+    queryset = Object.objects.all()
+    serializer_class = ObjectSerializer
